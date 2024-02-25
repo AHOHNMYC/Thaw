b5ed251795d684b4ab6fd0aff96cb678d30539f5
AHOHNMYC <24810600+AHOHNMYC@users.noreply.github.com>
2024-02-25 18:19:44 +0300

    42

--- src/frost/crypt/FrostCrypt.java
+++ src/frost/crypt/FrostCrypt.java
@@ -38,7 +38,7 @@ import org.bouncycastle.crypto.generators.*;
 import org.bouncycastle.crypto.params.*;
 import org.bouncycastle.crypto.signers.*;
 import org.bouncycastle.jce.provider.*;
-import org.bouncycastle.util.encoders.*;
+//import org.bouncycastle.util.encoders.*;
 
 /**
  * Implementation of the crypto layer.
